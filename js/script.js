206b4ed81949c360ea0de5b986739c2f44307b5a
kh123 <kelvinahuynh@gmail.com>
2020-05-28 03:16:51 -0400

    New project page, nav bar current page breadcrumb

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let links;
+
+// function currentNav(element) {
+
+//     console.log(element);
+    
+//     links = document.getElementsByTagName('a');
+
+//     for (let i = 0; i < links.length; i++) {
+//         links[0].classList.remove('active-nav');
+//     }
+
+//     element.classList.add('active-nav');
+// }
