b95a2290dcaae0597a9c790ef0321c306c782971
kh123 <kelvinahuynh@gmail.com>
2020-07-03 20:32:12 -0400

    edits to contact form code

--- js/script.js
+++ js/script.js
@@ -1,61 +1,3 @@
-// for contact.html
-function validateForm() {
-    var name =  document.getElementById('name').value;
-    if (name == "") {
-        document.querySelector('.status').innerHTML = "Name cannot be empty";
-        return false;
-    }
-    var email =  document.getElementById('email').value;
-    if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
-        return false;
-    } else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
-            return false;
-        }
-    }
-    var subject =  document.getElementById('subject').value;
-    if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
-        return false;
-    }
-    var message =  document.getElementById('message').value;
-    if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
-        return false;
-    }
-    document.querySelector('.status').innerHTML = "Sending...";
-  }
-
-// FOR CONTACT FORM
-// document.getElementById('status').innerHTML = "Sending...";
-// formData = {
-// 'name'     : $('input[name=name]').val(),
-// 'email'    : $('input[name=email]').val(),
-// 'subject'  : $('input[name=subject]').val(),
-// 'message'  : $('textarea[name=message]').val()
-// };
-
-
-// $.ajax({
-// url : "mail.php",
-// type: "POST",
-// data : formData,
-// success: function(data, textStatus, jqXHR)
-// {
-
-// $('#status').text(data.message);
-// if (data.code) //If mail was sent successfully, reset the form.
-// $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-// },
-// error: function (jqXHR, textStatus, errorThrown)
-// {
-// $('#status').text(jqXHR);
-// }
-// });
-
 // smooth anchor scrolling
 document.querySelectorAll('a[href^="#"][href$="Anchor"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -85,16 +27,16 @@ window.addEventListener('scroll', function(e) {
     if (window.pageYOffset === 0) {
         removeAllActiveNavClasses();
         document.querySelector('.homeNav').classList.add('active-nav');
-    } else if (window.pageYOffset >= 620 && window.pageYOffset < 1400) {
+    } else if (window.pageYOffset >= 594 && window.pageYOffset < 1379) {
         removeAllActiveNavClasses();
         document.querySelector('.aboutNav').classList.add('active-nav');
-    } else if (window.pageYOffset >= 1400 && window.pageYOffset < 2000) {
+    } else if (window.pageYOffset >= 1380 && window.pageYOffset < 2000) {
         removeAllActiveNavClasses();
         document.querySelector('.projectsNav').classList.add('active-nav');
-    } else if (window.pageYOffset >= 2000 && window.pageYOffset < 2710) {
+    } else if (window.pageYOffset >= 2001 && window.pageYOffset < 2770) {
         removeAllActiveNavClasses();
         document.querySelector('.photosNav').classList.add('active-nav');
-    } else if (window.pageYOffset >= 2710) {
+    } else if (window.pageYOffset >= 2771) {
         removeAllActiveNavClasses();
         document.querySelector('.contactNav').classList.add('active-nav');
     }
